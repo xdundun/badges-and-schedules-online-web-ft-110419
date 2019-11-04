c38d05a3618d20d48a1430b73c0479d7c36cda76
Dardan Gjonbalaj <xdundun@gmail.com>
2019-11-04 18:51:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,9 @@
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(array)
+  batch_array = []
+  counter = 0
+  array.each do |names|
+    batch_array[counter] = "Hello, my name is #{names}."
